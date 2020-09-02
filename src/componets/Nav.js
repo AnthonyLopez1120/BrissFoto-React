364fb7f9b3e6955069a111f9a9607bef977ac25a
Anthony Lopez <anthonybeconscious@gmail.com>
2020-09-02 17:01:51 -0500

    styled navbar. still needs fixes

--- src/componets/Nav.js
+++ src/componets/Nav.js
@@ -1,16 +1,53 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import styled from "styled-components"
+
+const NavWrapper = styled.header`
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-content: center;
+    padding: 20px 20px 20px 20px;
+    position: sticky;
+    top: 0;
+    width: 100%;
+    box-shadow: 0px 5px 5px #c4c4c4;
+    height: 270px;
+    flex-wrap:wrap;
+`;
+
+const StyledLink = styled(Link)`
+    text-decoration:none;
+    color: black
+`;
+const Imgdiv = styled.div`
+ height:200px;
+ width: 100%;
+
+`;
+
+const Logo = styled.img`
+    
+    max-width: 100%; 
+    max-height: 100%; 
+   
+`;
 
 
 function Nav () {
 
     return(
     <>
-        <Link to="/">Home</Link>
-        <Link to="/Blog">Blog</Link>
-        <Link to="/About">About</Link>
-        <Link to="/Contact">Contact</Link>
-
+    <NavWrapper>
+        <Imgdiv>
+            <Logo src={require("../images/logo.png")} alt='logo for briceida flor'/>
+        </Imgdiv>
+        <StyledLink to="/">Home</StyledLink>
+        <StyledLink to="/Blog">Blog</StyledLink>
+        <StyledLink to="/About">About</StyledLink>
+        <StyledLink to="/Contact">Contact</StyledLink>
+    </NavWrapper>
     </>
     )
 }
